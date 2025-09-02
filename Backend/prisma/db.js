71625d1b1988551a0bf485b6ef776eea0d55634d
kbm-sketch <kbretmanna@gmail.com>
2025-09-02 11:40:53 -0500

    db.js

--- Backend/prisma/db.js
+++ Backend/prisma/db.js
@@ -5,8 +5,6 @@ const globalForPrisma = globalThis;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+
 
 module.exports = prisma;
