736ec1cee9adf3598626c7dbabf97142d2d11f7e
kbm-sketch <kbretmanna@gmail.com>
2025-09-03 08:37:43 -0500

    added header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 // import { useEffect, useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { health } from './services/api';
-import AppRoutes from './routes/AppRoutes.jsx';
+import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   // const [msg, setMsg] = useState('Checking API...');
   // useEffect(() => { health().then(d => setMsg(d.status)).catch(()=>setMsg('API not reachable')); }, []);
   return (
-     <div>
+    <div className="container py-5">
+      <h1 className="text-center text-secondary mb-4">LifeStyle Tracker</h1>
       <AppRoutes />
     </div>
-
   );
 }
