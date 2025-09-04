e445f37df045e79814232c0ff46b6eddc693e41d
Roseanna O. <purposely.us@gmail.com>
2025-09-03 21:48:07 -0500

    patient progress bar done, need to incorporate our backend

--- Frontend/src/components/PDash.jsx
+++ Frontend/src/components/PDash.jsx
@@ -3,6 +3,8 @@ import react from 'react';
 import Psidebar from './Psidebar';
 import PHeader from './PHeader';
 import Pcard from './Pcard';
+import Progress from './Pprogress';
+// import backProgress from '../apis/ProgressApi';
 
 const PDash = () => {
     return ( 
@@ -14,15 +16,19 @@ const PDash = () => {
 
             <div className="dashboard-view mx-4 p-4 bg-light rounded shadow">
              <div className="mb-4 ">
-               
-                
+       
                 <PHeader />
                  <Pcard />
-              
-            </div>
+            </div>        
+           
+           
+            {/* <Progress progress={60} /> */}
             
         </div>
-            
+        <div className="progressbar mb-4">
+             <h2 className="prog-title mb-4">Your Recovery Progress</h2> 
+            <Progress progress={60}  /> 
+        </div>
                 {/* <p className="text-muted">Backend says: {msg}</p> */}
     </div>
      );
